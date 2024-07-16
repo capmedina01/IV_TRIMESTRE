8be28c8ce5a2b1e91a399e5f37a6dbc06768a1a4
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-07-15 22:48:02 -0500

    prueba de carga

---
+++ petShop/petShop/src/main/java/com/petShopAPII/SaludoController.java
@@ -0,0 +1,22 @@
+package com.petShopAPII;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/saludar")
+public class SaludoController {
+
+
+
+@GetMapping("/hola")
+    public String saludar(){
+        return "Hola a todos";
+    }
+@GetMapping("/adios")
+    public String despedir(){
+        return "Nos Veremos";
+    }
+}
